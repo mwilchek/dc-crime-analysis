ae6cd0dba5710a7404cc6950ab5eb8700ebbfc90
Matt Wilchek <mwilchek@gmu.edu>
2020-06-20 21:56:07 -0400

    Initial commit

---
+++ Preprocessing Code/preprocess.R
@@ -0,0 +1,72 @@
+library(lubridate)
+
+# Read in the data scripted from 2010-2020/06
+Crime_Incidents_in_2010 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2010.csv")
+Crime_Incidents_in_2010$Year = 2010
+
+Crime_Incidents_in_2011 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2011.csv")
+Crime_Incidents_in_2011$Year = 2011
+
+Crime_Incidents_in_2012 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2012.csv")
+Crime_Incidents_in_2012$Year = 2012
+
+Crime_Incidents_in_2013 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2013.csv")
+Crime_Incidents_in_2013$Year = 2013
+
+Crime_Incidents_in_2014 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2014.csv")
+Crime_Incidents_in_2014$Year = 2014
+
+Crime_Incidents_in_2015 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2015.csv")
+Crime_Incidents_in_2015$Year = 2015
+
+Crime_Incidents_in_2016 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2016.csv")
+Crime_Incidents_in_2016$Year = 2016
+
+Crime_Incidents_in_2017 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2017.csv")
+Crime_Incidents_in_2017$Year = 2017
+
+Crime_Incidents_in_2018 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2018.csv")
+Crime_Incidents_in_2018$Year = 2018
+
+Crime_Incidents_in_2019 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2019.csv")
+Crime_Incidents_in_2019$Year = 2019
+
+Crime_Incidents_in_2020 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2019.csv")
+Crime_Incidents_in_2020$Year = 2020
+
+dc_crime <- rbind(Crime_Incidents_in_2010,
+                  Crime_Incidents_in_2011,
+                  Crime_Incidents_in_2012,
+                  Crime_Incidents_in_2013,
+                  Crime_Incidents_in_2014,
+                  Crime_Incidents_in_2015,
+                  Crime_Incidents_in_2016,
+                  Crime_Incidents_in_2017,
+                  Crime_Incidents_in_2018,
+                  Crime_Incidents_in_2019,
+                  Crime_Incidents_in_2020)
+
+# Check for missing reate for each column
+sapply(dc_crime, function(x) sum(is.na(x)))
+
+# Only keep interested columns in the dataframe
+df = dc_crime[, (names(dc_crime) %in% c("CCN", "REPORT_DAT", "SHIFT", "METHOD", "OFFENSE", "WARD",
+                                        "DISTRICT", "PSA", "NEIGHBORHOOD_CLUSTER",  "LATITUDE", 
+                                        "LONGITUDE", "Year"))]
+
+df_clean = na.omit(df)
+# Check type of each column
+str(df_clean)
+# Correct the type of date
+df_clean$WARD = as.factor(df_clean$WARD)
+df_clean$DISTRICT = as.factor(df_clean$DISTRICT)
+df_clean$REPORT_DAT = ymd_hms(df_clean$REPORT_DAT)   
+df_clean$Month = month(ymd_hms(df_clean$REPORT_DAT))
+df_clean$Day = day(ymd_hms(df_clean$REPORT_DAT))
+str(df_clean)
+
+setwd("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data")
+write.csv(df_clean, file = "dc_crime_clean.csv")
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# dc-crime-analysis
-Final project for GWU DATS 6401 - Data Visualization of Complex Data
+# DC_Crime-Analysis
+
+**DATA Description**
+
+DC crime incident locations. The dataset contains a subset of locations and attributes of incidents reported in the ASAP (Analytical Services Application) crime report database by the District of Columbia Metropolitan Police Department (MPD). I extracted incident reported from 2009/1/1 to 2019/10/31. Here I listed attribute used for visualization and analysis. 
+
+
+| Attribute               | Description                                                      | 
+|:----------------------- |:---------------------------------------------------------------- |
+| CCN                     | Criminal Complaint Number - unique to each report                | 
+| REPORT_DAT              | The date/time the offense was reported                           |
+| SHIFT                   | The duty shift that responded the call.                          |
+| METHOD                  | The category of the method used to commit the crime.             | 
+| OFFENSE                 | The category of crime committed.                                 | 
+| WARD                    | The political Ward identifier.                                   |
+| DISTRICT                | The police district.                                             |
+| PSA                     | Police Service Area                                              | 
+| NEIGHBORHOOD_CLUSTER    | Neighborhood Clusters                                            |
+| LATITUDE                | Latitude geographical data                                       |
+| LONGITUDE               | Longitude geographical data                                      |
+| Month                   | Month of the year                                                |
+| Day                     | Day of the month                                                 |
+
+==============================================================
