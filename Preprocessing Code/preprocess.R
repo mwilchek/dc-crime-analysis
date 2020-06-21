478b9102efdb37e94c6f2f2dc9197a20e77cc061
mwilchek <mwilchek@gmu.edu>
2020-06-21 00:16:21 -0400

    revised tabbed backgrounds

--- Preprocessing Code/preprocess.R
+++ Preprocessing Code/preprocess.R
@@ -1,37 +1,37 @@
 library(lubridate)
 
 # Read in the data scripted from 2010-2020/06
-Crime_Incidents_in_2010 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2010.csv")
+Crime_Incidents_in_2010 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2010.csv")
 Crime_Incidents_in_2010$Year = 2010
 
-Crime_Incidents_in_2011 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2011.csv")
+Crime_Incidents_in_2011 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2011.csv")
 Crime_Incidents_in_2011$Year = 2011
 
-Crime_Incidents_in_2012 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2012.csv")
+Crime_Incidents_in_2012 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2012.csv")
 Crime_Incidents_in_2012$Year = 2012
 
-Crime_Incidents_in_2013 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2013.csv")
+Crime_Incidents_in_2013 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2013.csv")
 Crime_Incidents_in_2013$Year = 2013
 
-Crime_Incidents_in_2014 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2014.csv")
+Crime_Incidents_in_2014 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2014.csv")
 Crime_Incidents_in_2014$Year = 2014
 
-Crime_Incidents_in_2015 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2015.csv")
+Crime_Incidents_in_2015 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2015.csv")
 Crime_Incidents_in_2015$Year = 2015
 
-Crime_Incidents_in_2016 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2016.csv")
+Crime_Incidents_in_2016 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2016.csv")
 Crime_Incidents_in_2016$Year = 2016
 
-Crime_Incidents_in_2017 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2017.csv")
+Crime_Incidents_in_2017 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2017.csv")
 Crime_Incidents_in_2017$Year = 2017
 
-Crime_Incidents_in_2018 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2018.csv")
+Crime_Incidents_in_2018 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2018.csv")
 Crime_Incidents_in_2018$Year = 2018
 
-Crime_Incidents_in_2019 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2019.csv")
+Crime_Incidents_in_2019 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2019.csv")
 Crime_Incidents_in_2019$Year = 2019
 
-Crime_Incidents_in_2020 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/DC_Crime-Analysis-master/DC_Crime-Analysis-master/data/Crime_Incidents_in_2019.csv")
+Crime_Incidents_in_2020 <- read.csv("F:/Data Science/Data Visualization/DATS 6401 - Final Project/Data/Crime_Incidents_in_2019.csv")
 Crime_Incidents_in_2020$Year = 2020
 
 dc_crime <- rbind(Crime_Incidents_in_2010,
